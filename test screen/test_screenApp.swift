1f0322e7eff3b558c03c30ad8d34cd5f336abf03
Vladimir Lukoyanov <78410480+Lado-lukoyanov@users.noreply.github.com>
2024-01-30 20:02:21 +0400

    Initial Commit

---
+++ test screen/test_screenApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_screenApp.swift
+//  test screen
+//
+//  Created by Владимир  Лукоянов on 30.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct test_screenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
